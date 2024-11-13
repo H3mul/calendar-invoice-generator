7c82365e9edce1c6a19cc0818a0aa90f3430ddd0
Phillip Dykman <phil.d324@gmail.com>
2024-11-12 18:38:19 -0800

    Implement sheet generation

--- CalendarToSheetsSync.ts
+++ CalendarToSheetsSync.ts
@@ -42,23 +42,91 @@ function fetchCalendarData(): Map<string, CalendarData> {
     
     const dateRange = getEventDateRange();
 
-    const calendar = calendars[0];
 
-    // calendars.forEach(calendar => {
+    // DEBUG: temporarily only use first calendar instead of all
+    // const calendar = calendars[0];
+    calendars.forEach(calendar => {
         invoiceData.set(
             calendar.getId(),
             { calendar, events: calendar.getEvents(...dateRange) }
         );
-
-        console.log(`Calendar: ${calendar.getName()}`);
-        console.log(JSON.stringify(
-            invoiceData.get(calendar.getId()).events.map(e => e.getTitle())
-        , null, 2));
-
-    // });
+    });
     return invoiceData;
+}
+
+function createMonthlyInvoiceSheet() {
+    const dateRange = getEventDateRange();
+    const fileName = `Monthly Calendar Summary ${dateRange[0].toLocaleDateString()} - ${dateRange[1].toLocaleDateString()}`;
+
+    const folder = DriveApp.getFolderById(settings.MONTHLY_SHEET_FOLDER_ID);
+    const sheet = SpreadsheetApp.create(fileName);
+    const file = DriveApp.getFileById(sheet.getId());
+    return sheet
+}
+
+function formatTimeString(dateSomething: any) {
+    return new Date(dateSomething).toLocaleTimeString(settings.TIME_LOCALE, settings.TIME_FORMAT);
+}
+
+function generateMonthlyInvoiceSheet(invoiceData: Map<string, CalendarData>) {
+    // DEBUG: temporarily grab test sheet instead of creating new one
+    // const spreadsheet = createMonthlyInvoiceSheet();
+    const spreadsheet = SpreadsheetApp.openById(settings.MONTHLY_SHEET_TEST_FILE_ID);
+
+    // DEBUG: temporarily delete all other sheets
+    spreadsheet.getSheets()
+        .filter(s => s.getName() !== "Notes")
+        .forEach(s => spreadsheet.deleteSheet(s));
+
+    const notesSheet = spreadsheet.getActiveSheet();
+
+    // DEBUG: temporarily clear sheet
+    notesSheet.clear();
+    notesSheet.setName("Notes");
+    notesSheet.getRange("A1").setValue(`This Sheet has been created using a periodic App Script`)
+    notesSheet.getRange("A2").setValue(`on ${new Date().toString()}`);
+
+    const headings = [ "Day", "Timestamps", "Event Title", "Hours Count", "Include In Total" ];
+
+    invoiceData.values().forEach(d => {
+        const data:Array<Array<any>> = [headings];
+
+        d.events
+            .filter(e => !e.isAllDayEvent())
+            .forEach(e => {
+                data.push([
+                    e.getStartTime().toLocaleDateString(),
+                    `${formatTimeString(e.getStartTime())}-${formatTimeString(e.getEndTime())}`,
+                    `${e.getTitle()}`,
+                    `${(e.getEndTime().getTime() - e.getStartTime().getTime()) / (1000 * 60 * 60)}`,
+                    "TRUE"
+                ]);
+            });
+
+        // Append hours total row
+        data.push(["", "", "Total: ", `=SUM(ARRAYFORMULA(D2:D${data.length}*E2:E${data.length}))`, ""]);
+
+
+        const sheet = spreadsheet.insertSheet(d.calendar.getName());
+
+        // Add data
+        sheet.getRange(1,1, data.length, data[0].length).setValues(data);
+
+        // Add validation to the inclusion column (note: assume its the last column)
+        sheet.getRange(2, headings.length, data.length-2, 1).setDataValidation(SpreadsheetApp.newDataValidation().requireCheckbox().build());
+
+        // Set first and last rows to bold
+        const boldStyle = SpreadsheetApp.newTextStyle().setBold(true).build();
+        sheet.getRange(1,1, 1, data[0].length).setTextStyle(boldStyle);
+        sheet.getRange(data.length,1, 1, data[0].length).setTextStyle(boldStyle);
+
+        sheet.setColumnWidths(1, headings.length, 100);
+        // Make Event Title column a bit wider
+        sheet.setColumnWidth(3, 175);
+    });
 }
 
 function onTrigger() {
     const invoiceData = fetchCalendarData();
+    generateMonthlyInvoiceSheet(invoiceData);
 }
